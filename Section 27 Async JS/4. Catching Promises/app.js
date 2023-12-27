122ae14939385825ed907cf23d565b7d40f81886
Armando G <115781553+axmsj@users.noreply.github.com>
2023-12-26 23:58:38 -0500

    commit

--- Section 27 Async JS/4. Catching Promises/app.js
+++ Section 27 Async JS/4. Catching Promises/app.js
@@ -38,26 +38,65 @@ delayedColorChange('red. 1000')
   .then(() => delayedColorChange('violet', 1000))
   .then(() => delayedColorChange('white', 1000))
 
-const h1 = document.querySelector('#header')
+//My Practice Below
 
-const delayedColorHeader = (color, delay) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      h1.style.color = color
-      resolve()
-    }, delay)
-  })
-}
+// const h1 = document.querySelector('#header')
 
-delayedColorHeader('violent', 1000).then(() =>
-  delayedColorHeader('indigo', 1000)
-    .then(() => delayedColorHeader('blue', 1000))
-    .then(() => delayedColorHeader('green', 1000))
-    .then(() => delayedColorHeader('yellow', 1000))
-    .then(() => delayedColorHeader('orange', 1000))
-    .then(() => delayedColorHeader('red', 1000))
-)
-// My practice changing header by creating a new Promise with asynchronous js
+// const delayedColorHeader = (color, delay) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       h1.style.color = color
+//       resolve()
+//     }, delay)
+//   })
+// }
+
+// delayedColorHeader('violent', 1000).then(() =>
+//   delayedColorHeader('indigo', 1000)
+//     .then(() => delayedColorHeader('blue', 1000))
+//     .then(() => delayedColorHeader('green', 1000))
+//     .then(() => delayedColorHeader('yellow', 1000))
+//     .then(() => delayedColorHeader('orange', 1000))
+//     .then(() => delayedColorHeader('red', 1000))
+//     .then(() => delayedColorHeader('black', 1000))
+// )
+
+// const button = document.querySelector('#btn')
+
+// button.addEventListener('click', function (e) {
+//   const buttonColorChange = (color, delay) => {
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         document.body.style.backgroundColor = color
+//         resolve()
+//       }, delay)
+//     })
+//   }
+//   buttonColorChange('red', 0)
+//     .then(() => buttonColorChange('orange', 1000))
+//     .then(() => buttonColorChange('yellow', 1000))
+//     .then(() => buttonColorChange('green', 1000))
+//     .then(() => buttonColorChange('blue', 1000))
+// })
+
+// const button2 = document.querySelector('#btn2')
+
+// button2.addEventListener('click', e => {
+//   const btnColorChange = color => {
+//     document.body.style.backgroundColor = color
+//   }
+//   btnColorChange('blue')
+// })
+
+//reset to white background
+// const reset = document.querySelector('#btnReset')
+
+// reset.addEventListener('click', e => {
+//   const resetBackground = color => {
+//     document.body.style.backgroundColor = color
+//   }
+//   resetBackground('white')
+// })
 
 // const header = document.querySelector('h1');
 
@@ -83,3 +122,22 @@ delayedColorHeader('violent', 1000).then(() =>
 //     });
 //   });
 // });
+
+// const h2 = document.querySelector('#header2')
+
+// const delayedColorHeader2 = (color, delay) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       h2.style.color = color
+//       resolve()
+//     }, delay)
+//   })
+// }
+
+// delayedColorHeader2('blue', 1000)
+//   .then(() => delayedColorHeader2('purple', 1000))
+//   .then(() => delayedColorHeader('green', 1000))
+//   .then(() => delayedColorHeader2('yellow', 1000))
+//   .then(() => delayedColorHeader2('orange', 1000))
+//   .then(() => delayedColorHeader2('red', 1000))
+//   .then(() => delayedColorHeader2('black', 1000))
