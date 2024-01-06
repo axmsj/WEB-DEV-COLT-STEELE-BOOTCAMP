cfda1fb9380a007bfd017f19b6e39e3618066e02
Armando G <115781553+axmsj@users.noreply.github.com>
2024-01-05 23:09:02 -0500

    29 changes

--- Section 29 Prototypes, Classes, & OOP/1. /1. OOP/app.js
+++ Section 29 Prototypes, Classes, & OOP/1. /1. OOP/app.js
@@ -32,29 +32,117 @@
 
 // const firstColor = makeColor(255, 255, 255);
 
-function Color(r, g, b) {
-  this.r = r;
-  this.g = b;
-  this.b = b;
+// function Color(r, g, b) {
+//   this.r = r;
+//   this.g = g;
+//   this.b = b;
+// }
+
+// Color.prototype.rgb = function () {
+//   const { r, g, b } = this;
+//   return `rgb(${r}, ${g}, ${b})`;
+// };
+
+// Color.prototype.rgba = function (a = 1.0) {
+//   const { r, g, b } = this;
+//   return `rgb(${r}, ${g}, ${b}, ${a})`;
+// };
+
+// Color.prototype.hex = function () {
+//   const { r, g, b } = this;
+//   return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+// };
+
+// const color1 = new Color(40, 255, 60);
+// const color2 = new Color(0, 0, 0);
+
+const body = document.querySelector('body');
+
+class Color {
+  constructor(r, g, b, name) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.name = name;
+  }
+  innerRGB() {
+    const { r, g, b } = this;
+    return `${r}, ${g}, ${b}`;
+  }
+  rgb() {
+    return `rgb(${this.innerRGB()})`;
+  }
+  rgba(a = 1.0) {
+    return `rgba(${this.innerRGB()}, ${a})`;
+  }
+  hex() {
+    const { r, g, b } = this;
+    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+  }
+  calcHSL() {
+    let { r, g, b } = this;
+
+    r /= 255;
+    g /= 255;
+    b /= 255;
+
+    //find greatest and smallest channel values
+    let cmin = Math.min(r, g, b),
+      cmax = Math.max(r, g, b),
+      delta = cmax - cmin,
+      h = 0,
+      s = 0,
+      l = 0;
+    if (delta == 0) h = 0;
+    else if (cmax == r)
+      //red is max
+      h = ((g - b) / delta) % 6;
+    else if (cmax == g) h = (b - r) / delta + 2;
+    else h = (r - g) / delta + 4;
+
+    h = Math.round(h * 60);
+
+    if (h < 0) h += 360;
+
+    l = (cmax + cmin) / 2;
+
+    s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+
+    s = +(s * 100).toFixed(1);
+    l = +(s * 100).toFixed(1);
+    this.h = h;
+    this.s = s;
+    this.l = l;
+  }
 }
 
-Color.prototype.rgb = function () {
-  const { r, g, b } = this;
-  return `rgb(${r}, ${g}, ${b})`;
-};
+const red = new Color(255, 67, 89, 'tomato');
+const white = new Color(255, 255, 255, 'white');
 
-Color.prototype.hex = function () {
-  const { r, g, b } = this;
-  return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-};
+class Car {
+  constructor(make, model, year, djhsj) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.djhsj = djhsj;
+  }
+  make() {
+    return `${make}`;
+  }
+  model() {
+    return `The model of the vehicle is ${this.model} `;
+  }
+  year() {
+    return `The car was made in ${this.year}`;
+  }
+  makeModelYear() {
+    const { make, model, year } = this;
+    return `The make of the vehicle is ${this.make}, and model is ${this.model}, year: ${this.year}`;
+  }
+}
 
-Color.prototype.rgba = function (a = 1.0) {
-  const { r, g, b } = this;
-  return `rgba(${r}, ${g}.${b}, ${a})`;
-};
-
-const color1 = new Color(30, 40, 50);
-const color2 = new Color(64, 82, 198);
+const firstCar = new Car('Subaru', 'WRX', 2017);
+const secCar = new Car('Dodge', 'Charger SRT', 2023);
 
 // function Word(text) {
 //   this.text = text;
