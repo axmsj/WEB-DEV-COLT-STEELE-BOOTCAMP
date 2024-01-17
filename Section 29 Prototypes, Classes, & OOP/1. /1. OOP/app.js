4ad789eab39484cfd8c47f3c6e15f1d918ba7c1f
Armando G <115781553+axmsj@users.noreply.github.com>
2024-01-16 23:02:22 -0500

    final changes to section 29

--- Section 29 Prototypes, Classes, & OOP/1. /1. OOP/app.js
+++ Section 29 Prototypes, Classes, & OOP/1. /1. OOP/app.js
@@ -64,6 +64,7 @@ class Color {
     this.g = g;
     this.b = b;
     this.name = name;
+    this.calcHSL();
   }
   innerRGB() {
     const { r, g, b } = this;
@@ -79,14 +80,27 @@ class Color {
     const { r, g, b } = this;
     return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
   }
+  hsl() {
+    const { h, s, l } = this;
+    return `hsl(${h}, ${s}%, ${l}%)`;
+  }
+  fullSaturation() {
+    const { h, l } = this;
+    return `hsl(${h},100%, ${l}%)`;
+  }
+  opposite() {
+    const { h, s, l } = this;
+    const newHue = (h + 180) % 360;
+    return `hsl(${newHue}, ${s}%, ${l}%)`;
+  }
   calcHSL() {
     let { r, g, b } = this;
-
+    // Make r, g, and b fractions of 1
     r /= 255;
     g /= 255;
     b /= 255;
 
-    //find greatest and smallest channel values
+    // Find greatest and smallest channel values
     let cmin = Math.min(r, g, b),
       cmax = Math.max(r, g, b),
       delta = cmax - cmin,
@@ -95,21 +109,29 @@ class Color {
       l = 0;
     if (delta == 0) h = 0;
     else if (cmax == r)
-      //red is max
+      // Red is max
       h = ((g - b) / delta) % 6;
-    else if (cmax == g) h = (b - r) / delta + 2;
+    else if (cmax == g)
+      // Green is max
+      h = (b - r) / delta + 2;
+    // Blue is max
     else h = (r - g) / delta + 4;
 
     h = Math.round(h * 60);
 
+    // Make negative hues positive behind 360°
     if (h < 0) h += 360;
-
+    // Calculate lightness
     l = (cmax + cmin) / 2;
 
+    // Calculate saturation
     s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
 
+    // Multiply l and s by 100
     s = +(s * 100).toFixed(1);
-    l = +(s * 100).toFixed(1);
+    l = +(l * 100).toFixed(1);
+
+    // Assign h, s, l to the object instance so that we can reuse it.
     this.h = h;
     this.s = s;
     this.l = l;
@@ -118,13 +140,13 @@ class Color {
 
 const red = new Color(255, 67, 89, 'tomato');
 const white = new Color(255, 255, 255, 'white');
+const orange = new Color(230, 126, 34, 'carrot');
 
 class Car {
   constructor(make, model, year, djhsj) {
     this.make = make;
     this.model = model;
     this.year = year;
-    this.djhsj = djhsj;
   }
   make() {
     return `${make}`;
