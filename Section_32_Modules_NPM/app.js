7b8c9ef6c29bbc4b42670cef841cdc5c005f9884
Armando G <115781553+axmsj@users.noreply.github.com>
2024-01-26 00:08:37 -0500

    commit

---
+++ Section_32_Modules_NPM/app.js
@@ -0,0 +1,8 @@
+const { PI, square } = require('./math');
+// console.log(PI);
+
+// console.log(square(9));
+
+const cats = require('./CatShelter');
+
+console.log('REQUIRED AN ENTIRE DIRECTORY', cats);
